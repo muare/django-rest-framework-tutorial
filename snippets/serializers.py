4406b83c1e2bfdc7d64b61ec7afd56a1bb9e96d3
xiebian <xiebianwhu@gmail.com>
2021-09-17 11:41:42 +0800

    add put,delete permission per user

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,15 +2,21 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
+
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'highlighted','linenos', 'language', 'style','owner']
+        fields = [
+            'id', 'title', 'code', 'highlighted', 'linenos', 'language',
+            'style', 'owner'
+        ]
+
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all())
 
     class Meta:
         model = User
